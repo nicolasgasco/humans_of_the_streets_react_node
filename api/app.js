517526bf86a7ee9479c72ea1e820a7a43ae95e90
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-11 12:40:35 +0200

    Refactored code with Prettier

--- api/app.js
+++ api/app.js
@@ -12,21 +12,21 @@ require("dotenv").config();
 // Bcrypt for password encryption
 const bcrypt = require("bcrypt");
 
-
 // Database initialization
 const MongoClient = require("mongodb").MongoClient;
 
-MongoClient.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@sandbox.1ybr6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+MongoClient.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@sandbox.1ybr6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   { useUnifiedTopology: true },
-  function(err, client, res) {
-    
-    if ( err != null ) {
-        res.send(err);
-    } 
+  function (err, client, res) {
+    if (err != null) {
+      res.send(err);
+    }
 
     app.locals.db = client.db("bootcamp_project2");
     console.log(`Connected to database...`);
-});
+  }
+);
 
 // Express middleware
 app.use(express.static("public"));
@@ -50,39 +50,39 @@ const passport = require("passport");
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 const LocalStrategy = require("passport-local").Strategy;
 
 passport.use(
-  new LocalStrategy(
-    { usernameField: "email" },
-    function (email, password, done) {
+  new LocalStrategy({ usernameField: "email" }, function (
+    email,
+    password,
+    done
+  ) {
+    app.locals.db
+      .collection("users")
+      .find({ email: email })
+      .toArray(function (err, users) {
+        if (users.length === 0) {
+          return done(null, false);
+        }
+        const user = users[0];
 
-      app.locals.db.collection("users")
-        .find({ email: email })
-        .toArray(function (err, users) {
-          if (users.length === 0) {
-            return done(null, false);
-          }
-          const user = users[0];
-          
-          if ( bcrypt.compareSync(password, user.password ) ) {
-            return done(null, user);
-          } else {
-            return done(null, false);
-          }
-        });
-    }
-  )
+        if (bcrypt.compareSync(password, user.password)) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+  })
 );
 
 passport.serializeUser(function (user, done) {
   done(null, user.email);
 });
 
-
 passport.deserializeUser(function (email, done) {
-  app.locals.db.collection("users")
+  app.locals.db
+    .collection("users")
     .find({ email: email })
     .toArray(function (err, users) {
       if (users.length === 0) {
@@ -96,14 +96,13 @@ passport.deserializeUser(function (email, done) {
 
 // Route to check if user is logged
 app.get("/api/check", (req, res) => {
-  if ( req.session.passport ) {
-    console.log("Logged: ", req.session.passport)
-    res.send( { isLogged: true } );
+  if (req.session.passport) {
+    console.log("Logged: ", req.session.passport);
+    res.send({ isLogged: true });
     return;
   }
-  console.log("Logged: ", req.session.passport)
-  res.send( { isLogged: false } );
-
+  console.log("Logged: ", req.session.passport);
+  res.send({ isLogged: false });
 });
 
 app.post("/api/signin", function (req, res) {
@@ -112,48 +111,62 @@ app.post("/api/signin", function (req, res) {
   const name = req.body.name;
   const surname = req.body.surname;
   const dateCreation = new Date();
-  
 
-  app.locals.db.collection("users")
-    .findOne({ "email": email }, (err, user) => {
-
-      if ( !user ) {
-        app.locals.db.collection("users").insertOne(
-          {
-            name,
-            surname,
-            email,
-            password,
-            date_creation: dateCreation
-          },
-          (err, respuesta) => {
-
-            if (err !== null) {
-              res.send({  new_user: false, success: false, msg: "An error occurred: " + err });
-            } else {
-              res.send({ new_user: true, success: true, msg: "User was registered" });
-            }
+  app.locals.db.collection("users").findOne({ email: email }, (err, user) => {
+    if (!user) {
+      app.locals.db.collection("users").insertOne(
+        {
+          name,
+          surname,
+          email,
+          password,
+          date_creation: dateCreation,
+        },
+        (err, respuesta) => {
+          if (err !== null) {
+            res.send({
+              new_user: false,
+              success: false,
+              msg: "An error occurred: " + err,
+            });
+          } else {
+            res.send({
+              new_user: true,
+              success: true,
+              msg: "User was registered",
+            });
           }
-        );
-      } else {
-        res.send({ new_user: false, msg: "User is already registered" });
-      }
-    });
+        }
+      );
+    } else {
+      res.send({ new_user: false, msg: "User is already registered" });
+    }
+  });
 });
 
-app.post("/api/login",
-    passport.authenticate("local", {
-      successRedirect: "/success",
-      failureRedirect: "/fail",
-    })
+app.post(
+  "/api/login",
+  passport.authenticate("local", {
+    successRedirect: "/success",
+    failureRedirect: "/fail",
+  })
 );
 
 app.get("/success", (req, res) => {
-    res.send({ loginDataCorrect: true, msg: "Login successful", user: req.user, session: true });
+  res.send({
+    loginDataCorrect: true,
+    msg: "Login successful",
+    user: req.user,
+    session: true,
+  });
 });
 
 app.get("/fail", (req, res) => {
-res.send({ loginDataCorrect: false, msg: "Wrong user or password", session: false });
+  res.send({
+    loginDataCorrect: false,
+    msg: "Wrong user or password",
+    session: false,
+  });
 });
 
 app.put("/api/logout", (req, res) => {
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,103 +5,100 @@ const { ObjectID } = require("bson");
 // Bcrypt for password encryption
 const bcrypt = require("bcrypt");
 
-
 // MIddleware for cyphering passwords
 function cypherPasswords(req, res, next) {
-    let user = req.body;
-    user.password = bcrypt.hashSync(user.password, 10);
-    req.body = user;
-    next();
+  let user = req.body;
+  user.password = bcrypt.hashSync(user.password, 10);
+  req.body = user;
+  next();
 }
 
-
 // Get user with given id
-router.get("/id/:id", ( req, res ) => {
+router.get("/id/:id", (req, res) => {
+  let db = req.app.locals.db;
 
-    let db = req.app.locals.db;
+  id = new ObjectID(req.params.id);
 
-    id = new ObjectID(req.params.id)
+  db.collection("users").findOne({ _id: id }, (err, user) => {
+    if (err !== null) {
+      res.send(err);
+    }
 
-    db.collection("users").findOne( { "_id": id },  (err, user ) => {
-        if ( err !== null ) {
-            res.send(err);
-        }
-        
-        if ( !user ) {
-            res.send( { "userExists": false, msg: "Database is empty" } );
-        }
+    if (!user) {
+      res.send({ userExists: false, msg: "Database is empty" });
+    }
 
-        res.send( { "userExists": true, results: user } )
-    });
+    res.send({ userExists: true, results: user });
+  });
 });
-
 
 // Get user with given email, this is POST because mail is needed
-router.post("/email/", ( req, res ) => {
+router.post("/email/", (req, res) => {
+  let db = req.app.locals.db;
 
-    let db = req.app.locals.db;
+  email = req.body.email;
 
-    email = req.body.email;
+  db.collection("users").findOne({ email: email }, (err, user) => {
+    if (err !== null) {
+      res.send(err);
+    }
 
-    db.collection("users").findOne( { "email": email },  (err, user ) => {
-        if ( err !== null ) {
-            res.send(err);
-        }
-        
-        if ( !user ) {
-            res.send( { "userFound": false, msg: "Database is empty" } );
-            return;
-        }
+    if (!user) {
+      res.send({ userFound: false, msg: "Database is empty" });
+      return;
+    }
 
-        res.send( { "userFound": true, results: user } )
-    });
+    res.send({ userFound: true, results: user });
+  });
 });
-
 
 // Update password of user specific ID
-router.put("/password", cypherPasswords, ( req, res ) => {
+router.put("/password", cypherPasswords, (req, res) => {
+  let db = req.app.locals.db;
 
-    let db = req.app.locals.db;
+  const userId = new ObjectID(req.body._id);
+  const newPassword = req.body.password;
+  const currentDate = new Date();
 
-    const userId = new ObjectID(req.body._id);
-    const newPassword = req.body.password;
-    const currentDate = new Date();
+  db.collection("users").updateOne(
+    { _id: userId },
+    { $set: { password: newPassword, password_modified: currentDate } },
+    (err, info) => {
+      if (err !== null) {
+        res.send(err);
+      }
 
+      if (!info.result.nModified === 0) {
+        res.send({ msg: "No entry was modified" });
+        return;
+      }
 
-    db.collection("users").updateOne( { "_id": userId }, { $set: { "password": newPassword, "password_modified": currentDate } },  (err, info ) => {
-        if ( err !== null ) {
-            res.send(err);
-        }
-
-        if ( !info.result.nModified === 0 ) {
-            res.send( { "msg": "No entry was modified"} );
-            return;
-        }
-
-        res.send( { nModified: info.result.nModified } )
-    });
+      res.send({ nModified: info.result.nModified });
+    }
+  );
 });
-
 
 // Modify personal data (name, surname, email)
-router.put("/update/data", ( req, res ) => {
+router.put("/update/data", (req, res) => {
+  let db = req.app.locals.db;
 
-    let db = req.app.locals.db;
-    
-    const oldmail = req.body.oldmail;
-    const email = req.body.email;
-    const name = req.body.name;
-    const surname = req.body.surname;
-    const dataModified = new Date();
+  const oldmail = req.body.oldmail;
+  const email = req.body.email;
+  const name = req.body.name;
+  const surname = req.body.surname;
+  const dataModified = new Date();
 
-    db.collection("users").updateOne( {"email": oldmail}, { $set: { email, name, surname, data_modified : dataModified } }, (err, result ) => {
-        if ( err !== null ) {
-            res.send(err);
-        }
-        
-        res.send( { results: result } )
-    });
+  db.collection("users").updateOne(
+    { email: oldmail },
+    { $set: { email, name, surname, data_modified: dataModified } },
+    (err, result) => {
+      if (err !== null) {
+        res.send(err);
+      }
+
+      res.send({ results: result });
+    }
+  );
 });
-
 
 module.exports = router;
