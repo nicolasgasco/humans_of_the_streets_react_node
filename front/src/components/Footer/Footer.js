471836f71f941893ab5157f6f00cca3e82ddf4e2
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-11 23:34:27 +0200

    Added footer component

---
+++ front/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <small className="made-by">Made by Nicolas Gasco.</small>
+    </footer>
+  );
+}
+
+export default Footer;
