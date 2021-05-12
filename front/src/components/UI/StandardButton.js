664e0677c40c7b3f40b164653091f5dd10e48085
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-12 19:41:40 +0200

    Put aside this component for now

---
+++ front/src/components/UI/StandardButton.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import "./StandardButton.css";
+
+function StandardButton(props) {
+  return (
+    <a className="btn btn-2">Hovedr</a> 
+  );
+}
+
+export default StandardButton;
