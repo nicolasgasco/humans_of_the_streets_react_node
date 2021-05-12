5660a75fd7d89b73cb191e31d159cf1703e20ca4
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-12 19:42:07 +0200

    Added component for home page content

---
+++ front/src/components/Main/HomeContent.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import MainWrapper from "../UI/MainWrapper";
+
+const HomeContent = () => {
+  return (
+      <MainWrapper>
+        <h1>Read the stories of those who live on the street</h1>
+        <p>
+          Join a community of volunteers dedicated to helping homeless people
+          around the world.
+        </p>
+        <p>You can start making a difference right now.</p>
+      </MainWrapper>
+  );
+};
+
+export default HomeContent;
