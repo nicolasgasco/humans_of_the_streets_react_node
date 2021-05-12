172c83f02b49055cc37836ecd9a7470e699e796d
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-12 22:55:35 +0200

    Added wrapper to add contrasting background

--- front/src/components/Main/HomeContent.js
+++ front/src/components/Main/HomeContent.js
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
+import ColoredWrapper from "../UI/ColoredWrapper";
 import MainWrapper from "../UI/MainWrapper";
 
 const HomeContent = () => {
   return (
-      <MainWrapper>
+    <MainWrapper>
+      <ColoredWrapper>
         <h1>Read the stories of those who live on the street</h1>
         <p>
           Join a community of volunteers dedicated to helping homeless people
           around the world.
         </p>
         <p>You can start making a difference right now.</p>
-      </MainWrapper>
+      </ColoredWrapper>
+    </MainWrapper>
   );
 };
 
