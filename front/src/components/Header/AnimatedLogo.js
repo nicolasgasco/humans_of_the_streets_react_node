7fb752b0a54f68aecbb6258bc7b0c10d0ecf98fd
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-12 13:59:42 +0200

    Added animation to home page logo

--- front/src/components/Header/AnimatedLogo.js
+++ front/src/components/Header/AnimatedLogo.js
@@ -1,27 +1,19 @@
 import "./AnimatedLogo.css";
 
-function AnimatedLogo() {
+const AnimatedLogo = () => {
   return (
-  // <div>
-  //   <h1 className="main-logo">Humans <span>of the</span> Streets</h1>
-  //   <div class="wrapper">
-  //     <div class="typing-demo">Write their story.</div>
-  //   </div>
-  // </div>
-
-<div className="animated-title">
-<div className="text-top">
-  <div>
-    <span>Humans of</span>
-    <span>the Streets</span>
-  </div>
-</div>
-<div className="text-bottom">
-  <div style={{"font-size": "1.5rem"}}>Read their stories...</div>
-</div>
-</div>
-  )
-  
-}
+    <div className="animated-title">
+      <div className="text-top">
+        <div>
+          <span>Humans</span>
+          <span>of the Streets</span>
+        </div>
+      </div>
+      <div className="text-bottom">
+        <div style={{ "font-size": "1.8rem" }}>Read their stories...</div>
+      </div>
+    </div>
+  );
+};
 
 export default AnimatedLogo;
