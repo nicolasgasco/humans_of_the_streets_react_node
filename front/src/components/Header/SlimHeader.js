b77bfa449e0b85b0f4456042b01d0d2854358f1f
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-12 19:41:03 +0200

    Added secondary header for pages other than home

---
+++ front/src/components/Header/SlimHeader.js
@@ -0,0 +1,14 @@
+import MainLogo from "./MainLogo";
+import UserIcon from "../UI/UserIcon";
+import "./SlimHeader.css";
+
+const SlimHeader = () => {
+  return (
+    <div className="slim-header">
+      <MainLogo />
+      <UserIcon />
+    </div>
+  );
+};
+
+export default SlimHeader;
