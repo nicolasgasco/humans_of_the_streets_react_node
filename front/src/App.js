a683d6e48f34b73a94bf7c1101c76bd7ae0fe0d8
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-12 13:58:42 +0200

    Added Route for browsing stories

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,24 @@
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import MainHeader from "./components/Header/MainHeader";
+import SlimHeader from "./components/Header/SlimHeader";
+import HomeContent from "./components/HomeMain/HomeContent";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <Route exact path="/">
         <MainHeader />
+        <HomeContent />
+      </Route>
+
+      <Route path="/browse">
+        <SlimHeader />
+        <h2>Browse their stories</h2>
       </Route>
       <Footer />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
