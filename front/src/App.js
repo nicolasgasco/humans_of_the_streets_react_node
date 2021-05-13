5048e9abeeaaa1438bc92e0bc60480ee5902db77
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-13 23:33:56 +0200

    Removed useless import statement

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import MainHeader from "./components/Header/MainHeader";
