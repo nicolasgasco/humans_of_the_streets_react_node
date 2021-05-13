d31f91840db0dff4e734a0b88e8fd57f32fb44bd
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-13 20:28:45 +0200

    Added logic to toggle login and signup forms

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,57 @@
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import MainHeader from "./components/Header/MainHeader";
 import SlimHeader from "./components/Header/SlimHeader";
+import LoginForm from "./components/Login/LoginForm";
+import SignupForm from "./components/Login/SignupForm";
+import AddContent from "./components/Main/AddContent";
 import BrowseContent from "./components/Main/BrowseContent";
 import HomeContent from "./components/Main/HomeContent";
+import StandardButton from "./components/UI/StandardButton";
 
 const App = () => {
+  const [session, setSession] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showSignup, setShowSignup] = useState(false);
+
+  const handleSession = (value) => {
+    setSession(value);
+  };
+
+  const toggleLoginFormVisibility = () => {
+    setShowLogin(!showLogin);
+    if (showSignup) {
+      setShowSignup(false);
+    } 
+  };
+
+  const toggleSignupFormVisibility = () => {
+    console.log("triggered@")
+    setShowSignup(!showSignup);
+    setShowLogin(!showLogin);
+  };
+
   return (
     <BrowserRouter>
-      <Route exact path="/">
-        <MainHeader />
-        <HomeContent />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <MainHeader toggleLoginFormVisibility={toggleLoginFormVisibility} />
+          {showLogin ? <LoginForm onLogin={handleSession} toggleSignupFormVisibility={toggleSignupFormVisibility} /> : null}
+          {showSignup ? <SignupForm /> : null}
+          <HomeContent />
+        </Route>
 
-      <Route path="/browse">
-        <SlimHeader />
-        <BrowseContent />
-      </Route>
+        <Route path="/browse">
+          <SlimHeader />
+          <BrowseContent />
+        </Route>
+
+        <Route path="/add">
+          <SlimHeader />
+          <AddContent />
+        </Route>
+      </Switch>
       <Footer />
     </BrowserRouter>
   );
