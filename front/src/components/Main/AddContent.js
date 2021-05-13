094693497c46ee6f493545946a93f808a5e52244
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-13 20:26:59 +0200

    Initizialized AddContent

---
+++ front/src/components/Main/AddContent.js
@@ -0,0 +1,12 @@
+import MainWrapper from "../UI/MainWrapper";
+
+// Main tag to add a new story
+const AddContent = () => {
+  return (
+    <MainWrapper>
+      <h2>Add a new story</h2>
+    </MainWrapper>
+  );
+};
+
+export default AddContent;
