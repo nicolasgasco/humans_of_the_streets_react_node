cf5ccde283fb4f0f694c62b9be9e0238dfd2cb27
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-13 20:20:36 +0200

    Added media query

--- front/src/components/Footer/Footer.js
+++ front/src/components/Footer/Footer.js
@@ -1,10 +1,9 @@
-import { Link } from "react-router-dom";
 import "./Footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
-      <small>Picture by <a href="https://www.pexels.com/es-es/@harrisonhaines?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels" target="_blank">Harrison Haines</a> on <a href="https://www.pexels.com/" target="_blank">Pexels</a>.</small>
+      <small>Picture by <a href="https://www.pexels.com/es-es/@harrisonhaines?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels" target="_blank" rel="noreferrer">Harrison Haines</a> on <a href="https://www.pexels.com/" target="_blank" rel="noreferrer">Pexels</a>.</small>
       <small className="made-by">Made by Nicolas Gasco.</small>
     </footer>
   );
