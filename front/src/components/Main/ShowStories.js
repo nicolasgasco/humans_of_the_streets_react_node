ea8cee8670dcf64ad70fd07f502cd0a673eb8ddf
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-14 17:15:35 +0200

    Tweaked style and HTML

--- front/src/components/Main/ShowStories.js
+++ front/src/components/Main/ShowStories.js
@@ -4,15 +4,13 @@ import "./ShowStories.css";
 // Container showing more single stories
 const ShowStories = ({ results }) => {
   const showResults = results.map((result) => {
-    return <ShowSingleStory key={result._id} story={result} />;
+    // Show only good results
+    if (result.approved !== "false") {
+      return <ShowSingleStory key={result._id} story={result} />;
+    }
   });
 
-  return (
-    <div>
-      <h2>Stories: </h2>
-      <div className="results-container">{showResults}</div>
-    </div>
-  );
+  return <div className="results-container">{showResults}</div>;
 };
 
 export default ShowStories;
