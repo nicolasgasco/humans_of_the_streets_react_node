422a8275c787c7e1abebce71039b7e205dd7acf2
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-14 17:13:52 +0200

    Added session logic

--- front/src/components/Header/MainNav.js
+++ front/src/components/Header/MainNav.js
@@ -1,14 +1,17 @@
-import { Link } from 'react-router-dom';
-import './MainNav.css';
+import { Link } from "react-router-dom";
+import "./MainNav.css";
 
 const MainNav = (props) => {
-    return (
-        <nav className="main-nav">
-            <Link to="/browse/">Browse</Link>
-            <button onClick={props.toggleLoginFormVisibility}>Login</button>
-        </nav>
-    );
-  }
-  
-  export default MainNav;
-  
+  return (
+    <nav className="main-nav">
+      <Link to="/browse/">Browse</Link>
+      {props.session ? (
+        <Link to="/profile/">Profile</Link>
+      ) : (
+        <button onClick={props.toggleLoginFormVisibility}>Login</button>
+      )}
+    </nav>
+  );
+};
+
+export default MainNav;
