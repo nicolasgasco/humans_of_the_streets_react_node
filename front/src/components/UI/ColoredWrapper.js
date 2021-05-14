434e02b69fe2be3d6fbe25947f1780061be265f9
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-14 17:10:49 +0200

    Moved files

--- front/src/components/UI/ColoredWrapper.js
+++
@@ -1,8 +0,0 @@
-import "./ColoredWrapper.css";
-
-// Wrapper to add contrasting background
-const ColoredWrapper = (props) => {
-  const classes = "colored-wrapper " + props.className;
-  return <div className={classes}>{props.children}</div>;
-};
-export default ColoredWrapper;
