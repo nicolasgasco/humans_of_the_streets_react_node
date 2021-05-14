8fef4805e738ce5a875fe6c4f0b995748cdc807f
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-14 17:11:50 +0200

    Added session logic

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import MainHeader from "./components/Header/MainHeader";
 import SlimHeader from "./components/Header/SlimHeader";
@@ -8,26 +8,32 @@ import SignupForm from "./components/Login/SignupForm";
 import AddContent from "./components/Main/AddContent";
 import BrowseContent from "./components/Main/BrowseContent";
 import HomeContent from "./components/Main/HomeContent";
+import ProfileContent from "./components/Main/ProfileContent";
 import StandardButton from "./components/UI/StandardButton";
 
 const App = () => {
-  const [session, setSession] = useState(false);
+  const [session, setSession] = useState(localStorage.getItem("logged"));
   const [showLogin, setShowLogin] = useState(false);
   const [showSignup, setShowSignup] = useState(false);
-
+  console.log(session);
   const handleSession = (value) => {
+    console.log(session);
     setSession(value);
+    if (value) {
+      localStorage.setItem("logged", true);
+    } else {
+      localStorage.removeItem("logged");
+    }
   };
 
   const toggleLoginFormVisibility = () => {
     setShowLogin(!showLogin);
     if (showSignup) {
       setShowSignup(false);
-    } 
+    }
   };
 
   const toggleSignupFormVisibility = () => {
-    console.log("triggered@")
     setShowSignup(!showSignup);
     setShowLogin(!showLogin);
   };
@@ -36,19 +42,33 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <MainHeader toggleLoginFormVisibility={toggleLoginFormVisibility} />
-          {showLogin ? <LoginForm onLogin={handleSession} toggleSignupFormVisibility={toggleSignupFormVisibility} /> : null}
+          <MainHeader
+            session={session}
+            toggleLoginFormVisibility={toggleLoginFormVisibility}
+          />
+          {showLogin ? (
+            <LoginForm
+              onLogin={handleSession}
+              toggleSignupFormVisibility={toggleSignupFormVisibility}
+              toggleLoginFormVisibility={toggleLoginFormVisibility}
+            />
+          ) : null}
           {showSignup ? <SignupForm /> : null}
           <HomeContent />
         </Route>
 
         <Route path="/browse">
-          <SlimHeader />
+          <SlimHeader session={session} />
           <BrowseContent />
         </Route>
 
-        <Route path="/add">
+        <Route path="/profile">
           <SlimHeader />
+          <ProfileContent />
+        </Route>
+
+        <Route path="/add">
+          <SlimHeader session={session} />
           <AddContent />
         </Route>
       </Switch>
