0436db07e72342d95dbe37092d8bcd5c5c9090ca
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-15 23:17:07 +0200

    Added logic to get current user info and delete one submitted story

--- front/src/App.js
+++ front/src/App.js
@@ -10,13 +10,32 @@ import BrowseContent from "./components/Main/BrowseContent";
 import HomeContent from "./components/Main/HomeContent";
 import ProfileContent from "./components/Main/ProfileContent";
 import StandardButton from "./components/UI/StandardButton";
+import UserMessage from "./components/UI/UserMessage";
 
 const App = () => {
   const [session, setSession] = useState(() => {
+    // One fetch to check if the user is logged, another fetch to get the ID
     fetch("/api/login/check")
       .then((res) => res.json())
       .then((result) => {
+        console.log("Session is ", result.isLogged);
         setSession(result.isLogged);
+        fetch("/api/users/email", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify({ email: result.user }),
+        })
+          .then((res) => res.json())
+          .then((result) => {
+            if (result.userFound) {
+              sessionStorage.setItem("user", result.results._id);
+            }
+          })
+          .catch(function (error) {
+            console.log("An error occurred: " + error.message);
+          });
       })
       .catch(function (error) {
         console.log("An error occurred: " + error.message);
@@ -24,16 +43,20 @@ const App = () => {
   });
   const [showLogin, setShowLogin] = useState(false);
   const [showSignup, setShowSignup] = useState(false);
+  const [modalMessage, setModalMessage] = useState(null);
+  const [storyToDelete, setStoryToDelete] = useState({
+    storyToDelete: { id: "", delete: false },
+  });
 
   const handleSession = (value) => {
-
     setSession(value);
-    if (value) {
-      localStorage.setItem("logged", true);
-    } else {
-      localStorage.removeItem("logged");
-    }
   };
+
+  useEffect(() => {
+    if (!session) {
+      sessionStorage.removeItem("user");
+    }
+  }, [session]);
 
   const toggleLoginFormVisibility = () => {
     setShowLogin(!showLogin);
@@ -47,19 +70,50 @@ const App = () => {
     setShowLogin(!showLogin);
   };
 
+  const handleModalMessage = (text) => {
+    setModalMessage(text);
+  };
+
+  const handleStoryToDelete = (value) => {
+    setStoryToDelete(value);
+  };
+
+  useEffect(() => {
+    if (storyToDelete.storyToDelete.id && storyToDelete.storyToDelete.delete) {
+      console.log(storyToDelete.storyToDelete.id);
+      fetch(`/api/humans/${storyToDelete.storyToDelete.id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          if (result.deletedCount > 0) {
+            setStoryToDelete({ storyToDelete: { id: "", delete: false } });
+          }
+        })
+        .catch(function (error) {
+          console.log("An error occurred: " + error.message);
+        });
+    }
+  }, [storyToDelete]);
+
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
+          {modalMessage ? (
+            <UserMessage onClick={handleModalMessage} text={modalMessage} />
+          ) : null}
           <MainHeader
             session={session}
             toggleLoginFormVisibility={toggleLoginFormVisibility}
+            handleModalMessage={handleModalMessage}
           />
           {showLogin ? (
             <LoginForm
               onLogin={handleSession}
               toggleSignupFormVisibility={toggleSignupFormVisibility}
               toggleLoginFormVisibility={toggleLoginFormVisibility}
+              handleModalMessage={handleModalMessage}
             />
           ) : null}
           {showSignup ? <SignupForm /> : null}
@@ -71,10 +125,27 @@ const App = () => {
           <BrowseContent />
         </Route>
 
-        <Route path="/profile">
-          <SlimHeader />
-          <ProfileContent />
-        </Route>
+        {session ? (
+          <Route path="/profile">
+            {modalMessage ? (
+              <UserMessage
+                onClick={handleModalMessage}
+                text={modalMessage}
+                cancelButton={true}
+                handleStoryToDelete={handleStoryToDelete}
+              />
+            ) : null}
+            <SlimHeader />
+            <ProfileContent
+              session={session}
+              handleModalMessage={handleModalMessage}
+              handleStoryToDelete={handleStoryToDelete}
+              storyToDelete={storyToDelete}
+            />
+          </Route>
+        ) : (
+          <Redirect to="/" />
+        )}
 
         <Route path="/add">
           <SlimHeader session={session} />
