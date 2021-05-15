e9266ec3fd45f4669eeb6f13abffb21500510451
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-15 23:18:55 +0200

    Added logic for modal message

--- front/src/components/Main/ProfileContent.js
+++ front/src/components/Main/ProfileContent.js
@@ -1,14 +1,16 @@
 import ProfileNav from "../UI/ProfileNav";
 import MainWrapper from "../UI/Wrappers/MainWrapper";
+import ShowSubmittedStories from "./ShowSubmittedStories";
 
 // Main container where stories are shown
-const ProfileContent = () => {
+const ProfileContent = (props) => {
   return (
     <MainWrapper>
       <h2>Your profile</h2>
       <h3>Navigate around the website</h3>
       <ProfileNav />
       <h3>Modify your data</h3>
+      <ShowSubmittedStories handleModalMessage={props.handleModalMessage} handleStoryToDelete={props.handleStoryToDelete} storyToDelete={props.storyToDelete}/>
     </MainWrapper>
   );
 };
