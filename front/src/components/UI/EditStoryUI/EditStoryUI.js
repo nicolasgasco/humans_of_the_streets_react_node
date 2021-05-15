4053bf26a73ee8c2b4111264082e7f8348d62618
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-15 23:23:49 +0200

    Added component to edit single story

---
+++ front/src/components/UI/EditStoryUI/EditStoryUI.js
@@ -0,0 +1,20 @@
+import "./EditStoryUI.css";
+
+const EditStoryUI = (props) => {
+  const clickDelete = () => {
+    props.handleModalMessage("Do you really want to delete this element?");
+    props.handleStoryToDelete((prevState) => {
+        let storyToDelete = Object.assign({}, prevState.storyToDelete);
+        storyToDelete["id"] = props.id;
+        return { storyToDelete };
+      });
+  };
+
+  return (
+    <div className="edit-story-UI">
+      <button onClick={clickDelete}>Delete</button>
+    </div>
+  );
+};
+
+export default EditStoryUI;
