19c20494230d64a745557267b3f9c674ee2fedaa
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-16 22:41:34 +0200

    Added logic for user panel

--- front/src/components/Main/AddContent.js
+++ front/src/components/Main/AddContent.js
@@ -1,12 +1,24 @@
+import UserPane from "../UI/UserPane";
 import MainWrapper from "../UI/Wrappers/MainWrapper";
 import AddStoryForm from "./AddStoryForm/AddStoryForm";
 
 // Main tag to add a new story
-const AddContent = () => {
+const AddContent = (props) => {
   return (
-    <MainWrapper>
-      <AddStoryForm />
-    </MainWrapper>
+    <>
+      <UserPane
+        linksToShow={[
+          ["Your profile", "/profile"],
+          ["Browse stories", "/browse"],
+          ["Homepage", "/"],
+        ]}
+        userPaneVisibility={props.showUserPane}
+        toggleUserPaneVisibility={props.toggleUserPaneVisibility}
+      />
+      <MainWrapper>
+        <AddStoryForm />
+      </MainWrapper>
+    </>
   );
 };
 
