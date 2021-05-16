8d7be4fa9f8d988c44db8811033de379f15c561c
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-16 22:32:53 +0200

    Added component for showing user panel

---
+++ front/src/components/UI/UserPane.js
@@ -0,0 +1,36 @@
+import "./UserPane.css";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const UserPane = (props) => {
+  const [paneVisiblity, setPaneVisiblity] = useState(false);
+  console.log(props.userPaneVisibility);
+
+  useEffect(() => {
+    setPaneVisiblity(props.userPaneVisibility);
+  }, [props.userPaneVisibility]);
+
+  const showLinks = props.linksToShow.map((link) => {
+    return (
+      <li>
+        <Link to={link[1]} className="user-pane-link" onClick={props.toggleUserPaneVisibility} >
+          {link[0]}
+        </Link>
+      </li>
+    );
+  });
+
+  return (
+    <>
+      <section
+        className="user-pane"
+        style={{ display: `${paneVisiblity ? "block" : "none"}` }}
+      >
+        <ul>{showLinks}</ul>
+      </section>
+
+    </>
+  );
+};
+
+export default UserPane;
