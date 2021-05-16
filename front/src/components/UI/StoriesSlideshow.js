45722f06999b6cfa8820c7f0e4150fd08a005f24
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-16 22:40:38 +0200

    Added Bootstrap slideshow for homepage

---
+++ front/src/components/UI/StoriesSlideshow.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import LoadingGif from "./img/loading.gif";
+import "./StoriesSlideshow.css";
+
+const StoriesSlideshow = () => {
+  const [homeStories, setHomeStories] = useState(() => {
+    fetch(`/api/humans`)
+      .then((res) => res.json())
+      .then((result) => {
+        setHomeStories(result.results.slice(0, 4));
+      })
+      .catch(function (error) {
+        console.log("An error occurred: " + error.message);
+      });
+  });
+
+  let showCarouselItems;
+  if (!homeStories) {
+    showCarouselItems = (
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carousel-img"
+          src={LoadingGif}
+          alt="Loading animation"
+        />
+        <Carousel.Caption>
+          <h3 style={{ color: "black" }}>Loading...</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    );
+  } else {
+    showCarouselItems = homeStories.map((story) => {
+      console.log(story);
+      return (
+        <Carousel.Item>
+          <img
+            className="d-block w-100 carousel-img"
+            src={story.img}
+            alt="First slide"
+          />
+          <Carousel.Caption className="carousel-caption">
+            <h3 style={{color: "white"}}>{`${story.name} ${story.surname} (${story.age})`}</h3>
+            <p style={{fontSize: "1.2rem", textAlign: "center"}}>{story.interview.advice}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      );
+    });
+  }
+
+  return (
+    <Carousel indicators={false} fade={true} interval={4000} className="carousel-container">
+      {showCarouselItems}
+    </Carousel>
+  );
+};
+
+export default StoriesSlideshow;
