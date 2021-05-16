db664ad80422295458a0dcfde5921289df2aac51
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-16 22:42:53 +0200

    Added slideshow

--- front/src/components/Main/HomeContent.js
+++ front/src/components/Main/HomeContent.js
@@ -3,24 +3,30 @@ import ColoredWrapper from "../UI/Wrappers/ColoredWrapper";
 import MainWrapper from "../UI/Wrappers/MainWrapper";
 import StandardButton from "../UI/StandardButton";
 import { Link } from "react-router-dom";
+import StoriesSlideshow from "../UI/StoriesSlideshow";
 
 // Text content shown on home
 const HomeContent = () => {
   return (
     <MainWrapper>
-      <div className="home-content">
-        <h1 className="home-content__allcaps-header">Humans of the Street</h1>
-        <h2 className="home-content__white-header">
-          Read the stories of those who live on the street
-        </h2>
-        <div className="home-content__paragraph-text">
-          <p>
-            Join a community of volunteers dedicated to helping homeless people
-            around the world.
-          </p>
-          <p>You can start making a difference right now.</p>
+      <div className="home-content__container">
+        <div className="home-content">
+          <h1 className="home-content__allcaps-header">Humans of the Street</h1>
+          <h2 className="home-content__white-header">
+            Read the stories of those who live on the street
+          </h2>
+          <div className="home-content__paragraph-text">
+            <p>
+              Join a community of volunteers dedicated to helping homeless
+              people around the world.
+            </p>
+            <p>You can start making a difference right now.</p>
+          </div>
+          <Link to="/browse/" className="standard-button">
+            Start&nbsp;browsing
+          </Link>
         </div>
-        <Link to="/browse/" className="standard-button">Start&nbsp;browsing</Link>
+        <StoriesSlideshow />
       </div>
     </MainWrapper>
   );
