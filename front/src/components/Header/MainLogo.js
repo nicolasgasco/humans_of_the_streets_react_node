52c6117f00576a420f38d8300d9786a8d419c3a5
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-17 14:19:37 +0200

    Fixed css modules class syntax

--- front/src/components/Header/MainLogo.js
+++ front/src/components/Header/MainLogo.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import "./MainLogo.css";
+import classes from "./MainLogo.module.css";
 
 // Regular logo for pages other than home
 const MainLogo = () => {
   return (
     <div>
-      <Link to="/"><h1 className="main-logo">
+      <Link to="/"><h1 className={classes["main-logo"]}>
         Humans <span>of</span><br /><span>the</span> Streets
       </h1></Link>
     </div>
