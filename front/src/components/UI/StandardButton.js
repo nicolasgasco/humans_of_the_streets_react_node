28bf874de4b7ddce6d04d732c74e513e657199cd
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-17 23:38:01 +0200

    Fixed class logic with modules

--- front/src/components/UI/StandardButton.js
+++ front/src/components/UI/StandardButton.js
@@ -3,7 +3,7 @@ import classes from "./StandardButton.module.css";
 
 function StandardButton(props) {
   return (
-    <Link to={props.link}><div className={`${classes["standard-button"]} ${props.className}`}>{props.text}</div></Link> 
+    <Link to={props.to}><div className={`${classes["standard-button"]} ${props.className}`}>{props.text}</div></Link> 
   );
 }
 
