1e30ea244e911ed92f992b78cc7d86a5aafb9ba8
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-17 14:21:02 +0200

    Fixed css modules class syntax

--- front/src/components/UI/StandardButton.js
+++ front/src/components/UI/StandardButton.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import "./StandardButton.css";
+import classes from "./StandardButton.module.css";
 
 function StandardButton(props) {
   return (
-    <Link to={props.link} className="standard-button">{props.text}</Link> 
+    <Link to={props.link}><div className={`${classes["standard-button"]} ${props.className}`}>{props.text}</div></Link> 
   );
 }
 
