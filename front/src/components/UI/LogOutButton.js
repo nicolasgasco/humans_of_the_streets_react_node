31a3ebb84d152d274df06e78c95a3e03f66e1c67
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-17 23:38:19 +0200

    Added styles

---
+++ front/src/components/UI/LogOutButton.js
@@ -0,0 +1,11 @@
+import StandardButton from "./StandardButton"
+
+const logoutUser = () => {
+    
+}
+
+const LogOutButton = () => {
+    return (
+        <StandardButton to="/" onClick={logoutUser}/>
+    )
+}
