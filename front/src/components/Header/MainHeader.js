b0eead2c5876b00c658f69d2597752b3b704c4d4
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-18 14:08:02 +0200

    Prepared build for deployment

--- front/src/components/Header/MainHeader.js
+++ front/src/components/Header/MainHeader.js
@@ -2,9 +2,18 @@ import MainNav from "./MainNav";
 import "./MainHeader.css";
 import AnimatedLogo from "./AnimatedLogo";
 import UserIcon from "../UI/UserIcon";
+import { useEffect } from "react";
 
 // Big header for homepage
 const MainHeader = (props) => {
+  useEffect(() => {
+    // This is to offset the search bar on mobile devices
+    // Get the viewport height and multiple it by 1% to get a value for a vh unit
+    const vh = window.innerHeight * 0.01;
+    // Det the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
+
   // Two different returns for mobile and desktop/tablet version
   if (window.innerWidth <= 600) {
     return (
@@ -12,7 +21,10 @@ const MainHeader = (props) => {
         <div className="picture-container">
           <AnimatedLogo />
         </div>
-        <MainNav session={props.session} toggleLoginFormVisibility={props.toggleLoginFormVisibility} />
+        <MainNav
+          session={props.session}
+          toggleLoginFormVisibility={props.toggleLoginFormVisibility}
+        />
       </header>
     );
   } else {
@@ -21,7 +33,10 @@ const MainHeader = (props) => {
         <div className="picture-container">
           <AnimatedLogo />
         </div>
-        <MainNav session={props.session} toggleLoginFormVisibility={props.toggleLoginFormVisibility} />
+        <MainNav
+          session={props.session}
+          toggleLoginFormVisibility={props.toggleLoginFormVisibility}
+        />
       </header>
     );
   }
