efb351ddb99b656b335e79ae4c59e8c00d60ce9a
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-18 22:38:05 +0200

    Added css modules

--- front/src/components/UI/WhiteButton.js
+++ front/src/components/UI/WhiteButton.js
@@ -2,6 +2,6 @@ import styles from "./WhiteButton.module.css";
 
 // Wrapper to style white/transparent buttons
 const WhiteButton = (props) => {
-  return <button type={props.type} onClick={props.onClick} className={styles["white-button"]}>{props.text}</button>;
+  return <button type={props.type} onClick={props.onClick} className={`${styles["white-button"]} ${props.className}`}>{props.text}</button>;
 };
 export default WhiteButton;
