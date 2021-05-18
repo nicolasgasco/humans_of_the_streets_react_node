dc388626d127d83cfde91c94d8ac4b4e16b3d62f
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-18 22:37:48 +0200

    Added css modules

--- front/src/components/UI/StoriesSlideshow.js
+++ front/src/components/UI/StoriesSlideshow.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import LoadingGif from "./img/loading.gif";
-import "./StoriesSlideshow.css";
+import classes from "./StoriesSlideshow.module.css";
 
 const StoriesSlideshow = () => {
   const [homeStories, setHomeStories] = useState(() => {
@@ -20,7 +20,7 @@ const StoriesSlideshow = () => {
     showCarouselItems = (
       <Carousel.Item key={"loading"}>
         <img
-          className="d-block w-100 carousel-img"
+          className={classes["d-block w-100 carousel-img"]}
           src={LoadingGif}
           alt="Loading animation"
         />
@@ -34,11 +34,11 @@ const StoriesSlideshow = () => {
       return (
         <Carousel.Item key={story._id}>
           <img
-            className="d-block w-100 carousel-img"
+            className={classes["d-block w-100 carousel-img"]}
             src={story.img}
             alt="First slide"
           />
-          <Carousel.Caption className="carousel-caption">
+          <Carousel.Caption className={classes["carousel-caption"]}>
             <h3 style={{color: "white"}}>{`${story.name} ${story.surname} (${story.age})`}</h3>
             <p style={{fontSize: "1.2rem", textAlign: "center"}}>{story.interview.advice}</p>
           </Carousel.Caption>
@@ -48,7 +48,7 @@ const StoriesSlideshow = () => {
   }
 
   return (
-    <Carousel indicators={false} fade={true} interval={4000} className="carousel-container">
+    <Carousel indicators={false} fade={true} interval={4000} className={classes["carousel-container"]}>
       {showCarouselItems}
     </Carousel>
   );
