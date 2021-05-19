0c60afed07b8d7d861ec85be995d0887fe94d816
nicolasgasco <nicolas.gasco92@gmail.com>
2021-05-19 11:22:49 +0200

    Changed to css modules and added rule to refresh page on orientation change

--- front/src/components/Header/AnimatedLogo.js
+++ front/src/components/Header/AnimatedLogo.js
@@ -1,17 +1,34 @@
-import "./AnimatedLogo.css";
+import classes from "./AnimatedLogo.module.css";
 
 // This animated logo requires this structure to work
 const AnimatedLogo = () => {
+
+  // Reload page if orientation is changed, otherwise amination doesn't show correctly
+  window.onorientationchange = function () {
+    var orientation = window.orientation;
+    switch (orientation) {
+      case 0:
+      case 90:
+      case -90:
+        window.location.reload();
+        break;
+    }
+  };
+
   return (
-    <div className="animated-title">
-      <div className="text-top">
+    <div className={classes["animated-title"]}>
+      <div className={classes["text-top"]}>
         <div>
-          <span className="main-text">Humans</span>
-          <span className="main-text"><span className="of-the-span">of the</span> Streets</span>
+          <span className={classes["main-text"]}>Humans</span>
+          <span className={classes["main-text"]}>
+            <span className={classes["of-the-span"]}>of the</span> Streets
+          </span>
         </div>
       </div>
-      <div className="text-bottom">
-        <div ><p className="read-stories-sub">Read their stories...</p></div>
+      <div className={classes["text-bottom"]}>
+        <div>
+          <p className={classes["read-stories-sub"]}>Read their stories...</p>
+        </div>
       </div>
     </div>
   );
